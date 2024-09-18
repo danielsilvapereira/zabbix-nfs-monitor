46cc1191556c48c5760ca2385cb749fdbaecf114
Daniel Pereira <60305462+danielsilvapereira@users.noreply.github.com>
2024-09-18 10:52:00 -0300

    Create nfs_discovery.sh

---
+++ nfs_discovery.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+array="$(findmnt -lo target -n -t nfs,nfs4)"
+
+comma=""
+printf '%s' '{"data":['
+
+while IFS= read -r line ; do
+        printf '%s' "$comma{\"{#MOUNT_POINT}\":\"$line\"}"
+        comma=","
+done <<< "$array"
+
+printf '%s' ']}'
